6041e777dd02e65c4bfec9694b94b1604a84a80f
AlfredoMarrero <a.e.marrero@hotmail.com>
2018-08-19 11:29:32 -0500

    Implement logic for car loan

--- CarLoanVB.Net/MainWindow.xaml.vb
+++ CarLoanVB.Net/MainWindow.xaml.vb
@@ -1,3 +1,20 @@
 ﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
 
+    End Sub
+
+    Private Sub btnAnalize_Click(sender As Object, e As RoutedEventArgs)
+        Dim loanAmount, monthlyInterest, anualInterestRate, monthlyPayment As Double
+        Dim months As Int32
+
+        If Double.TryParse(txtAmountOfLoan.Text, loanAmount) And Double.TryParse(txtInterestRate.Text, anualInterestRate) And Int32.TryParse(txtDuration.Text, months) Then
+            monthlyInterest = (anualInterestRate / 100) / 12
+            monthlyPayment = FormatCurrency(loanAmount * monthlyInterest / (1 - Math.Pow(1 + monthlyInterest, months * -1)))
+            txtMonthyPayments.Text = FormatCurrency(monthlyPayment)
+            txtInterestPaid.Text = FormatCurrency(months * monthlyPayment - loanAmount)
+            Return
+        End If
+
+
+    End Sub
 End Class
