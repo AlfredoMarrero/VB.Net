8314ff096b1f1d635ca1f4541b83ad323d21695a
AlfredoMarrero <a.e.marrero@hotmail.com>
2018-08-20 04:45:33 -0500

    Add View Controller and Model

---
+++ CarLoanWeb/Controllers/CarLoanController.cs
@@ -0,0 +1,31 @@
+﻿using CarLoanWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarLoanWeb.Controllers
+{
+    public class CarLoanController : Controller
+    {
+        // GET: CarLoan
+        public ActionResult Index()
+        {
+            var test = new CarLoanModel();
+            test.interestRate = 100;
+            return View(test);
+        }
+        [HttpPost]
+        public ActionResult Calculate(CarLoanModel carLoanModel)
+        {
+            ModelState.Clear();
+            carLoanModel.totalInterestPaid = 200;
+            carLoanModel.monthlyPayment = 20;
+            carLoanModel.interestRate = 0;
+            //Response.OutputStream.Write(writeBuffer, 0, count);
+            return View("Index", carLoanModel);
+        }
+
+    }
+}
---
+++ CarLoanWeb/Models/CarLoanModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarLoanWeb.Models
+{
+    public class CarLoanModel
+    {
+        public Double loanAmount { get; set; }
+        public int months { get; set; }
+        public Double interestRate {get; set;}
+        public Double totalInterestPaid { get; set; }
+        public Double monthlyPayment { get; set; }
+        public int durationInMonth { get; set; }
+
+    }
+}
